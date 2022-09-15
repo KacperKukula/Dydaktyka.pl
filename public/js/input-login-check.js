4c2e272be9be0fcf871d9be3e9ce1227ad86dc25
KacperKukula <50201673+KacperKukula@users.noreply.github.com>
2022-09-15 14:09:00 +0200

    +changed damn tailwind to sass, +completed login validation data 2/2

--- public/js/input-login-check.js
+++ public/js/input-login-check.js
@@ -1,9 +1,45 @@
 const login = document.querySelector("input#login")
 const passwd = document.querySelector("input#passwd")
+const errorLogin = document.querySelector(".login-error-name")
+const errorPasswd = document.querySelector(".login-error-passwd")
 
-login.addEventListener("focusout", ValidateInput())
+let ForbiddenSigns = [":", ";", " ", "-", "=", "+", "{", "}", "[", "]", "/", "\\", "|", "*", "@", "~"]
 
-function ValidateInput() {
-    login.value.split("")
-    
-}
+login.addEventListener("focusout", (e)=>{
+    let userInput = login.value.toLocaleLowerCase().split("")
+
+    try {
+        if(userInput.length<=3 || userInput>=32) {
+            throw 'To short input'
+        }
+        userInput.forEach((x)=>{
+            ForbiddenSigns.forEach((sign)=>{
+                if(sign==x) throw 'Forbidden sign used'
+            })
+        })
+        login.style.borderColor = "green"
+        errorLogin.style.display = "none"
+    }
+    catch(error) {
+        login.style.borderColor = "red"
+        errorLogin.innerHTML = error
+        errorLogin.style.display = "inline"
+    }
+})
+
+passwd.addEventListener("focusout", (e)=>{
+    let userInput = login.value.toLocaleLowerCase().split("")
+
+    try {
+        if(userInput.length<=3 || userInput>=32) {
+            throw 'To short input'
+        }
+        passwd.style.borderColor = "green"
+        errorPasswd.style.display = "none"
+    }
+    catch(error) {
+        passwd.style.borderColor = "red"
+        errorPasswd.innerHTML = error
+        errorPasswd.style.display = "inline"
+    }
+})
